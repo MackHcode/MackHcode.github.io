f4f22eee04336c263618d8638035eaf649f58b9e
MackHcode <qcup4105@gmail.com>
2025-09-24 11:37:44 -0400

    Add files via upload

---
+++ projects/js/chapter1/project02/project01-02_txt.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Mack Horton
+    Date:   9/23/25
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+	service1Name="Basic"
+	service2Name="Express"
+	service3Name="Extreme"
+	service4Name="Ultimate"
+	service1Speed="50 Mbps"
+	service2Speed="100 Mbps"
+	service3Speed="500 Mbps"
+	service4Speed="1 Gig"
