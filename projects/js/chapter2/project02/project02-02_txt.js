8a3ea380e57d2717a2f6834417515b3522a26fb5
MackHcode <qcup4105@gmail.com>
2025-10-22 12:15:55 -0400

    Add files via upload

---
+++ projects/js/chapter2/project02/project02-02_txt.js
@@ -0,0 +1,33 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: 
+      Date:   
+
+      Filename: project02-02.js
+ */
+ 
+function verifyForm() 
+{
+ 
+  let name = document.getElementById("name").value;
+
+  
+  let email = document.getElementById("email").value;
+
+  
+  let phone = document.getElementById("phone").value;
+
+  
+  if (name && email && phone) 
+      {
+      window.alert("Thank you!");
+      } 
+        else 
+       {
+      window.alert("Please fill in all fields");
+       }
+}     
+document.getElementById("submit").addEventListener("click", verifyForm);
