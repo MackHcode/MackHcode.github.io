5380c61e6b81e5e66a80893d461bb66e137db4b5
MackHcode <qcup4105@gmail.com>
2025-12-12 11:36:53 -0500

    Add files via upload

---
+++ projects/html/fitness11/scripts/scripts.js
@@ -0,0 +1,49 @@
+/*
+Mackenzie Horton
+Date dec 5, 2025 
+*/
+
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src")
+var descriptionSource = document.getElementById("despsrc");
+
+function burpees(){
+    videoSource.src = "media/burpees.mp4";
+    descriptionSource.src = "media/burpees-description.vtt";
+    video.style.display = "block";
+    video.load();
+}
+function plank(){
+    videoSource.src = "media/plank.mp4";
+    descriptionSource.src = "media/plank-description.vtt";
+    video.style.display = "block";
+    video.load();
+}
+function mountain(){
+    videoSource.src = "media/mountain.mp4";
+    descriptionSource.src = "media/mountain-description.vtt";
+    video.style.display = "block";
+    video.load();
+}
+function hamburger(){
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+
+    if(menu.style.display === "block" && 
+        logo.style.display === "none")
+        {
+        menu.style.display = "none";
+        logo.style.display = "block";
+        }
+        else
+        {
+        menu.style.display = "none";
+        logo.style.display = "block";
+        }
+}
+function discount(){
+    var promo = document.getElementById("special");
+    promo.firstChild.nodeValue = "Promo Code: D25START";
+    promo.style.color ="#ff0000";
+    promo.style.fontSize = "2em";
+}
