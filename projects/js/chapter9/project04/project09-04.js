8a554e6c6c13f53be1a37b893c0d7e0e486ba047
MackHcode <qcup4105@gmail.com>
2026-04-16 14:06:39 -0400

    Add files via upload

---
+++ projects/js/chapter9/project04/project09-04.js
@@ -0,0 +1,49 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-04
+
+      Project to store high scores from a game in a cookie
+      Author: 
+      Date:   
+
+      Filename: project09-04.js
+*/
+
+/* Page Objects */
+let bestText = document.getElementById("best");
+let clockTimer = document.getElementById("timer");
+
+// Custom event that runs when the puzzle is solved
+window.addEventListener("puzzleSolved", updateRecord);
+
+// Event listener that is run when the page loads
+window.addEventListener("load", function() {
+
+});
+
+function getBestTime() {
+      if (document.cookie) {
+
+      let cookieArray = document.cookie;
+      cookieArray = cookieArray.split("=");
+      return parseInt(cookieArray[1]);
+      }
+      else {
+            return 9999;
+      }
+}
+
+function updateRecord() {
+      let solutionTime = parseInt(document.getElementById("timer").value);
+      let bestTime = getBestTime();
+
+      if (solutionTime < bestTime) {
+            bestTime = solutionTime
+            bestText.textContent = bestTime + " seconds";
+
+
+      }
+      let maxAge = 7776000;
+      document.cookie = "puzzle8Best" +  bestTime + "maxage =" + 60*60*24*90;
+}
